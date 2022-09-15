8a7bbf2bab0912e68a46e851dd84830e26857885
Scheuer Mark <markscheuer2002@gmail.com>
2022-09-15 10:57:07 +0200

    Create Json helper

---
+++ src/Codecool.CodecoolShop/Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Codecool.CodecoolShop.Helpers
+{
+    public static class JsonHelper
+    {
+        public static void AppendJsonFile<T>(T data, string fileName)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            File.AppendAllText(json, $"./Data/${fileName}.json");
+        }
+
+        public static T ReadJsonFile<T>(string fileName)
+        {
+            string json = File.ReadAllText($"./Data/${fileName}.json");
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
